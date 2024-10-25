162a5d92238382dff9a50acebe9df868eb46f7e5
Pratik Kumar <pratikkr75@gmail.com>
2024-10-25 20:05:07 +0530

    Save local changes before merging third-branch

--- client/admin/src/App.jsx
+++ client/admin/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
   return (
     <>
       {/* <Navbar/> */}
-      {/* <Outlet/> */}
+      <Outlet/>
       {/* <Footer/> */}
       {/* <Home /> */}
-      {/* <SignupAdmin /> */}
-      <LoginAdmin />
+      <SignupAdmin />
+      {/* <LoginAdmin /> */}
     </>
   )
 }
--- server/database/db.js
+++ server/database/db.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const DB_PASSWORD = process.env.DB_PASSWORD; 
 const DB_URI = `mongodb+srv://brainwave76:${DB_PASSWORD}@cluster0.wooai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const URI = process.env.MONGO_URI;
+// const URI = process.env.MONGO_URI;
 const Connection = async () => {
     try {
-        await mongoose.connect(URI);
+        await mongoose.connect(DB_URI);
         console.log("Database connected successfully");
     } catch (err) {
         console.log("Error while connecting to database", err);
